5a46355e5598262f08e224c89a10c595bd8893db
purple-eye-orange <12sh20@naver.com>
2024-11-14 18:27:22 +0900

    first commit

---
+++ src/ GoogleLoginButton.jsx
@@ -0,0 +1,40 @@
+import { GoogleLogin } from "@react-oauth/google";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import axios from "axios";
+
+const GoogleLoginButton = () => {
+  const clientId = import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID;
+
+  const handleSuccess = async (res) => {
+    try {
+      const response = await axios.post(
+        "http://routinemonsterapi.seongjinemong.app/api/users/signin",
+        {
+          credential: res.credential,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log("백엔드로 성공적으로 전송:", response);
+    } catch (err) {
+      console.error("백엔드 전송 오류:", err);
+    }
+  };
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <GoogleLogin
+        onSuccess={handleSuccess}
+        onFailure={(err) => {
+          console.error("로그인 실패:", err);
+        }}
+      />
+    </GoogleOAuthProvider>
+  );
+};
+
+export default GoogleLoginButton;
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import GoogleLoginButton from "./ GoogleLoginButton";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Google Login Example</h1>
+      <GoogleLoginButton />
+    </>
+  );
+}
+
+export default App;
